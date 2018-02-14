9f65f4c5b930f0d887aad7aab42bf88282136baa
t8lv4 <morgan@t8lv4.fr>
2018-02-14 08:48:26 +0100

    Initial Commit

---
+++ terminalCombat/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  terminalCombat
+//
+//  Created by Morgan on 14/02/2018.
+//  Copyright © 2018 Morgan. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
