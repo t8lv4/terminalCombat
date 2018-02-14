4504356285c2c91a7be1d35905d4b5dacd8ac894
t8lv4 <morgan@t8lv4.fr>
2018-02-14 10:43:28 +0100

    Ability to give unique name to players

--- terminalCombat/main.swift
+++ terminalCombat/main.swift
@@ -1,6 +1,7 @@
 //
 //  main.swift
-//  terminalCombat
+//  terminalCombat, a combat game for two teams of three fighters.
+//      to be played on a terminal
 //
 //  Created by Morgan on 14/02/2018.
 //  Copyright © 2018 Morgan. All rights reserved.
@@ -8,5 +9,81 @@
 
 import Foundation
 
-print("Hello, World!")
+////////////////
+//
+//MARK: varaibles, functions, collections
+//
+////////////////
+
+//a buffer of names, to check for name's uniqueness in the game
+var buffer = ["", " "]
+
+//array of players names
+var playerName = [String]()
+
+
+
+////////////////
+//
+//MARK: classes
+//
+////////////////
+
+class Player {
+    
+    var nameOfPlayer: String?
+    /*
+    init() {
+        self.getPlayerName()
+    }
+    */
+    func getPlayerName() {
+        print("what's your name ?")
+        if let name = readLine() {
+            if buffer.contains(name) {
+                print("your choice is not valid or this name is already in use"
+                    + "\nplease choose another name")
+                getPlayerName()
+            } else {
+                nameOfPlayer = name
+                playerName.append(name)
+                buffer.append(name)
+            }
+        }
+    }
+
+}
+
+class setUp {
+    
+}
+
+class teamMember {
+    
+}
+
+
+///////////////
+//
+//MARK: main
+//
+//////////////
+
+//input player's names
+var player1 = Player()
+player1.getPlayerName()
+var player2 = Player()
+player2.getPlayerName()
+
+
+print("\(playerName[0]), \(playerName[1])")
+
+print("hello \(player1.nameOfPlayer!) and \(player2.nameOfPlayer!) !"
++ "\nwelcome to terminalCombat")
+
+
+
+
+
+
 
